f9ae5a79bae5209b45a14b5d4ed22f77f3488a1b
Langwedocjusz <mikolaj.pedro@gmail.com>
2021-08-11 15:07:45 +0200

    Added files to premake

--- premake5.lua
+++ premake5.lua
@@ -9,23 +9,32 @@ project "GLFW"
         "include/GLFW/glfw3.h",
         "include/GLFW/glfw3native.h",
         "src/glfw_config.h",
+        "src/internal.h",
+        "src/mappings.h",
         "src/context.c",
         "src/init.c",
         "src/input.c",
         "src/monitor.c",
+        "src/vulkan.c",
         "src/window.c",
 
+        "src/win32_platform.h",
         "src/win32_init.c",
+        "src/win32_joystick.h",
         "src/win32_joystick.c",
         "src/win32_monitor.c",
         "src/win32_time.c",
         "src/win32_thread.c",
+        "src/win32_window.c",
+        "src/wgl_context.h",
         "src/wgl_context.c",
+        "src/egl_context.h",
         "src/egl_context.c",
+        "src/osmesa_context.h",
         "src/osmesa_context.c"
     }
 
-    staticruntime "off"
+    staticruntime "on"
     systemversion "latest"
 
     defines {
@@ -34,12 +43,10 @@ project "GLFW"
     }
 
     filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
+        buildoptions "/MTd"
 
 	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+         buildoptions "/MT"
 
     filter "configurations:Distribution"
-		runtime "Release"
+        buildoptions "/MT"
