2121447158d582decc1b8cab598c28f6e9b92b03
Langwedocjusz <mikolaj.pedro@gmail.com>
2022-03-22 16:19:01 +0100

    Updated premake file

--- premake5.lua
+++ premake5.lua
@@ -8,29 +8,38 @@ project "GLFW"
     files {
         "include/GLFW/glfw3.h",
         "include/GLFW/glfw3native.h",
-        "src/glfw_config.h",
-        "src/internal.h",
-        "src/mappings.h",
+
         "src/context.c",
         "src/init.c",
         "src/input.c",
+        "src/internal.h",
+        "src/mappings.h",
         "src/monitor.c",
+        "src/platform.h",
+        "src/platform.c",
         "src/vulkan.c",
         "src/window.c",
 
-        "src/win32_platform.h",
+        "src/null_init.c",
+        "src/null_joystick.c",
+        "src/null_monitor.c",
+        "src/null_platform.h",
+        "src/null_window.c",
+
         "src/win32_init.c",
         "src/win32_joystick.h",
         "src/win32_joystick.c",
+        "src/win32_module.c",
         "src/win32_monitor.c",
-        "src/win32_time.c",
+        "src/win32_platform.h",
+        "src/win32_thread.h",
         "src/win32_thread.c",
+        "src/win32_time.h",
+        "src/win32_time.c",
         "src/win32_window.c",
-        "src/wgl_context.h",
+
         "src/wgl_context.c",
-        "src/egl_context.h",
         "src/egl_context.c",
-        "src/osmesa_context.h",
         "src/osmesa_context.c"
     }
 
